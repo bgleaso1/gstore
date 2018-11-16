0556eeffff651ca09400163ee55bdf572cebb75f
bgleaso1 <gleason.brandon@gmail.com>
2018-11-16 18:38:17 -0500

    Starting data processing - Added basic sample upload - Parsed "hits" column in new file (although this will get thrown out anyways)

---
+++ hits.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(jsonlite)
+library(stringr)
+
+setwd("C:/Users/gleas/Google Drive/Coding/R/")
+
+# Load data -----------
+
+gstore <-
+  read_csv(
+    file = "train_v2.csv",
+    col_names =  T,
+    n_max = 100 # only first 100 rows to sample
+  )
+
+# Helper functions -----------
+
+fix_json_string <-
+  function(s) {
+    s %>% # s is a string
+      str_replace_all(pattern = '"', replacement = "'") %>% 
+      str_replace_all(
+        pattern = "'(?![:alpha:])|(?<=[:space:]|[:punct:])'", 
+        # PATTERN: 
+        #    single quote followed by a letter 
+        #    OR single quote preceded by a space/punctuation
+        replacement = '"'
+      ) %>%
+      str_replace_all(pattern = "True", replacement = '"TRUE"') %>%
+      str_replace_all('" : "', '":"') %>%
+      str_replace_all('(?<!: )"(?= |é)', "")
+  }
+
+# Apply json string fix -----------
+
+gstore2 <-
+  gstore %>%
+  select(fullVisitorId, visitStartTime, hits) %>%
+  mutate(
+    fixed_json_string = fix_json_string(hits),
+    hits_object = map(fixed_json_string, fromJSON)
+  )
+
+gstore3 <-
+  gstore2 %>%
+  mutate(
+    page_hits = map(hits_object, "page"),
+    pages_pagePaths = map(page_hits, "pagePath")
+  ) 
+
--- main.R
+++ main.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 library(devtools)
+library(jsonlite)
+library(stringr)
+library(janitor)
 
-# script goes here
-# I'm Martin
+setwd("C:/Users/gleas/Google Drive/Coding/R/")
+
+# Data processing -----------
+
+gstore <-
+  read_csv(
+    file = "train_v2.csv",
+    col_names =  T,
+    n_max = 100 # only first 100 rows to sample
+  )
